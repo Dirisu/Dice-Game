d6bd14e1d6ecb1f4b330214e39f20e404251c0a0
Dirisu Marvellous <90407212+Dirisu@users.noreply.github.com>
2022-02-28 10:05:53 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+var randomImage = "images/dice" + randomNumber1 + ".png";
+
+document.querySelector(".img1").setAttribute("src", randomImage);
+
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+var randomImage2 = "images/dice" + randomNumber2 + ".png";
+
+document.querySelector(".img2").setAttribute("src", randomImage2);
+
+if (randomNumber1 > randomNumber2) {
+    document.querySelector("h1").innerHTML = "Player 1 Wins 🚩";
+}
+else if (randomNumber1 < randomNumber2) {
+    document.querySelector("h1").innerHTML = "🚩 Player 2 Wins";
+}
+
+else {
+    document.querySelector("h1").innerHTML = "Draw";
+}
